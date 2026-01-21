7b91c835c0286f7e8219a87f375fc51679bf8d89
Kaifuddn <159402186+Kaifuddn@users.noreply.github.com>
2026-01-21 09:31:05 -0800

    ddl_bronze.sql

--- Scripts/bronze/ddl_bronze.sql
+++ Scripts/bronze/ddl_bronze.sql
@@ -1,116 +1,90 @@
 /*
-================================================================================
+===============================================================================
 DDL Script: Create Bronze Tables
-================================================================================
-
+===============================================================================
 Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables
+    This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
-
-    Run this script to re-define the DDL structure of 'bronze' tables.
-
-================================================================================
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
-Use DataWareHouse;
+
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
 GO
 
-Create or alter procedure bronze.load_bronze as 
-BEGIN  
-	BEGIN TRY
-		DECLARE @batch_start_time datetime;
-		DECLARE @batch_end_time datetime;
-		DECLARE @start_time datetime;
-		DECLARE @end_time datetime;
-		SET @batch_start_time = getdate();
-		SET @start_time = getdate(); 
-		PRINT '=====================================';
-		PRINT 'Loading Bronze Layer';
-		PRINT '=====================================';
-		Print 'Truncating bronze.crm ';
-		Truncate table bronze.crm_cust_info  
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
 
-		bulk insert bronze.crm_cust_info
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-		with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);  
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
 
-		-- select count (*) from bronze.crm_prd_info
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
-		Truncate table bronze.crm_prd_info
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
 
-		bulk insert bronze.crm_prd_info
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-		-- select count (*) from bronze.crm_sales_details
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
 
-		Truncate table bronze.crm_sales_details
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-		bulk insert bronze.crm_sales_details
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
 
-		SET @end_time = getdate();
-		Print 'time it took was ' + cast(datediff ( second , @start_time , @end_time )as nvarchar) + ' seconds';
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-		-- select count (*) from bronze.crm_sales_details
-		Print 'Truncating bronze.erp ';
-		Truncate table bronze.erp_cust_az12
-
-		bulk insert bronze.erp_cust_az12
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-		with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);
-
-		-- select count (*) from bronze.erp_cust_az12
-
-		Truncate table bronze.erp_loc_a101
-
-		bulk insert bronze.erp_loc_a101
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-			with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);
-
-		-- select count (*) from bronze.erp_loc_a101
-
-		Truncate table bronze.erp_px_cat_g1v2
-
-		bulk insert bronze.erp_px_cat_g1v2
-		from 'C:\Users\kaifu\OneDrive\Documents\BA\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-		with (
-			firstrow = 2,
-			fieldterminator = ',',
-			tablock
-		);
-
-		-- select count (*) from bronze.erp_px_cat_g1v2
-
-		SET @batch_end_time = getdate();
-		Print  '----------------------------------------------------------------';
-		print 'it took '+ cast (datediff(second , @batch_start_time ,@batch_end_time) as nvarchar) +' seconds for the bronze layer to load';
-		Print  '----------------------------------------------------------------';
-	END TRY
-	BEGIN CATCH 
-		Print  '--------------------------------';
-		Print  'ERROR'+ ERROR_MESSAGE() + CAST( ERROR_NUMBER() AS Nvarchar);
-		Print  '--------------------------------';
-	END CATCH 
-END  
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
